642407cfbaa33be35cd941f7f691604973396b2a
shankarkasala <umashankar@gmail.com>
2023-07-27 20:18:31 +0530

    cart items page changes

--- src/Pages/CartItems/CartItems.js
+++ src/Pages/CartItems/CartItems.js
@@ -1,27 +1,155 @@
-import { Icon } from '@mui/material';
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  // Icon,
+  Typography,
+  Container,
+  Button,
+  styled,
+  CardActions,
+  // FormControl,
+  // InputLabel,
+  // Select,
+  // MenuItem,
+} from "@mui/material";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+// import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { cloneDeep } from "lodash";
+import { getSelectedProduct } from "../Product/Actions/ProductActions";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+import "./cartItems.css";
 
 function CartItems() {
-    const navigate = useNavigate();
-    const product = useSelector((state) => state.selectedProduct);
-    const handleBack = () => {
-        navigate("/");
-      };
-      console.log(product)
-  return (
-    <div>
-         <Icon
-              component={ArrowBackIcon}
-              sx={{ marginRight: "8px", cursor: "pointer" }}
-              onClick={handleBack}
-            />
+  // const [qty, setQty] = React.useState("");
 
-            Cart Items
-    </div>
-  )
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const AddButton = styled(Button)(({ theme }) => ({
+    marginLeft: "84px",
+    border: "1px solid #27374D",
+    color: "#fff",
+    fontSize: "12px",
+    background: "#27374D",
+    "&:hover": {
+      color: "#fff",
+      background: "#27374D",
+      fontSize: "12px",
+      border: "1px solid #27374D",
+    },
+  }));
+  const product = useSelector((state) => state.selectedProduct);
+  // const handleBack = () => {
+  //   navigate("/");
+  // };
+
+  const handleDeleteItem = (item) => {
+    let updateList = cloneDeep(product.selectedProduct);
+    let index = updateList.findIndex((res) => res._id === item._id);
+    updateList.splice(index, 1);
+    dispatch(getSelectedProduct(updateList));
+  };
+
+  // const handleChange = (event) => {
+  //   setQty(event.target.value);
+  // };
+
+  return (
+    <Container
+      sx={{ margin: "0 auto", width: "100%", margin: "24px", display: "flex" }}
+    >
+      <Container>
+        {product.selectedProduct.map((res) => {
+          return (
+            <Card
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginLeft: "20px",
+                marginBottom: "8px",
+                padding: "8px",
+                height: "150px",
+                width: "800px",
+                position: "relative",
+              }}
+            >
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography component="div" variant="h5">
+                    {res.title}
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Size: {res.availableSizes}
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                  >
+                    Price: <CurrencyRupeeIcon sx={{ fontSize: "14px" }} />{" "}
+                    {res.price}
+                  </Typography>
+                  {/* <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={qty}
+                    onChange={handleChange}
+                    label="Qty"
+                  >
+                    <MenuItem value="1">
+                      <em>1</em>
+                    </MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                  </Select>
+                </FormControl> */}
+                </CardContent>
+              </Box>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image={res.image}
+                alt={res.title}
+              />
+              <div className="deleteIcon" onClick={() => handleDeleteItem(res)}>
+                <DeleteOutlinedIcon sx={{ cursor: "pointer" }} />
+              </div>
+            </Card>
+          );
+        })}
+      </Container>
+      <Card
+        sx={{
+          marginBottom: "8px",
+          padding: "8px",
+          height: "330px",
+          width: "500px",
+        }}
+      >
+        <Container>
+          <Box>
+            <Typography>Total: 230</Typography>
+          </Box>
+        </Container>
+        <CardActions sx={{ margin: "8px" }}>
+          <AddButton size="small" variant="contained">
+            Proceed To Pay
+          </AddButton>
+        </CardActions>
+      </Card>
+    </Container>
+  );
 }
 
-export default CartItems
+export default CartItems;
--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -6,6 +6,11 @@ import {
   Typography,
   Alert,
   Rating,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from "@mui/material";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,9 +20,14 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import "./product.css";
 import { cloneDeep } from "lodash";
 import { getSelectedProduct } from "./Actions/ProductActions";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+
 function Product() {
   const [selectedSize, setSelectedSize] = useState("");
   const [successAlert, setSuccesAlert] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
+
   const [failAlert, setFailAlert] = useState(false);
   const [value, setValue] = React.useState(3);
 
@@ -48,7 +58,7 @@ function Product() {
 
   const CartButton = styled(Button)(({ theme }) => ({
     width: "120px",
-    marginTop: "180px",
+    marginTop: "150px",
     float: "right",
     padding: "8px",
     background: "#27374D",
@@ -64,7 +74,7 @@ function Product() {
 
   const CancelButton = styled(Button)(({ theme }) => ({
     width: "120px",
-    marginTop: "180px",
+    marginTop: "150px",
     float: "right",
     padding: "8px",
     background: "#DDE6ED",
@@ -90,22 +100,31 @@ function Product() {
   const handleSelectedItem = () => {
     if (selectedSize) {
       setSuccesAlert(true);
+      setOpenDialog(true);
       let selectedItem = cloneDeep(product?.product);
       selectedItem.availableSizes = [selectedSize];
       let cart = cloneDeep(cartItems.selectedProduct);
       cart.push(selectedItem);
       dispatch(getSelectedProduct(cart));
-      setTimeout(() => {
-        navigate("/cartItems");
-      }, 3000);
+      // setTimeout(() => {
+      //   navigate("/cartItems");
+      // }, 3000);
     } else {
       setFailAlert(true);
     }
   };
 
+  const handleGoCart = () => {
+    navigate("/cartItems");
+  };
+
   const handleCloseAlert = () => {
     setFailAlert(false);
     setSuccesAlert(false);
+  };
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
   };
 
   return (
@@ -131,13 +150,24 @@ function Product() {
                 {product?.product?.description}
               </Typography>
               <Rating
-              sx={{marginLeft:"16px"}}
+                sx={{ marginLeft: "16px" }}
                 name="simple-controlled"
                 value={value}
                 onChange={(event, newValue) => {
                   setValue(newValue);
                 }}
               />
+              <div className="price">
+                <Typography
+                  variant="subtitle1"
+                  color="text.secondary"
+                  component="div"
+                >
+                  Price: <CurrencyRupeeIcon sx={{ fontSize: "14px" }} />{" "}
+                  {product?.product?.price}
+                </Typography>
+              </div>
+
               <div className="sizeContainer">
                 {product?.product?.availableSizes.map((res) => (
                   <div onClick={() => handleSelectSize(res)}>
@@ -154,7 +184,7 @@ function Product() {
                 ))}
               </div>
               <CartButton onClick={handleSelectedItem} variant="contained">
-                Go to cart{" "}
+                Add to cart{" "}
                 <ShoppingCartIcon
                   sx={{ marginLeft: "4px", fontSize: "14px" }}
                 />
@@ -193,6 +223,26 @@ function Product() {
           </Alert>
         )}
       </Snackbar>
+      <Dialog
+        open={openDialog}
+        onClose={handleCloseAlert}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        fullWidth={true}
+        maxWidth={"xs"}
+      >
+        <DialogTitle id="alert-dialog-title">{"AWsome !!"}</DialogTitle>
+        <div className="dialogContent">
+          <CheckCircleIcon sx={{ fontSize: "38px", color: "green" }} />
+          <Typography>Item Added to cart!!</Typography>
+        </div>
+        <DialogActions>
+          <Button onClick={handleBack}>Add More</Button>
+          <Button onClick={handleGoCart} autoFocus>
+            Proceed
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 }
